563f571f6083aa4fda0548d9bd8dee377ef6c0b6
Brandon Arroyo <brandsharky@gmail.com>
2026-03-29 19:03:55 -0700

    Complete chamber discover page and initialize final folders and starter files

---
+++ chamber/data/places.mjs
@@ -0,0 +1,60 @@
+export const places = [
+  {
+    name: "Newport Beach Temple",
+    address: "2300 Bonita Canyon Dr",
+    description: "The Newport Beach California Temple is a serene house of worship dedicated in 2005. Featuring striking Spanish mission-style architecture, Salisbury pink granite, and lushly landscaped grounds, it offers a peaceful and quiet place for reflection and spiritual services.",
+    photo: "images/temple.webp",
+    link: "https://www.churchofjesuschrist.org/temples/details/newport-beach-california-temple?lang=eng",
+  },
+  {
+    name: "Newport Beach",
+    address: "Oceanfront and 21st Street",
+    description: "Newport Beach offers fine, soft sand with plenty of room to stretch out and enjoy yourself. Surfers, swimmers and sunbathers all bask in the warmth of beautiful Southern California winter and summer days. Pelicans and seagulls also enjoy Southern California’s year round Mediterranean climate.",
+    photo: "images/beach.webp",
+    link: "https://visitnewportbeach.com/beaches-and-parks/newport-municipal-beach/",
+  },
+  {
+    name: "Newport Pier",
+    address: "1 Newport Pier",
+    description: "Newport Pier offers fine, soft sand with plenty of room to stretch out and enjoy yourself. Surfers, swimmers and sunbathers all bask in the warmth of beautiful Southern California winter and summer days. Pelicans and seagulls also enjoy Southern California’s year-round Mediterranean climate, especially at Newport Beach Pier.",
+    photo: "images/pier.webp",
+    link: "https://visitnewportbeach.com/beaches-and-parks/newport-pier-located-balboa-peninsula/",
+  },
+  {
+    name: "Newport Harbor",
+    address: "720 W Bay Ave",
+    description: "Newport Harbor is a semi-artificial harbor that was formed in the early 1900s after the Newport Bay estuary was dredged. With approximately 9,000 boats, Newport Harbor is one of the largest recreational boat harbors on the west coast. Newport Beach Harbor is a popular stop for sailing, fishing, rowing, canoeing, kayaking and more.",
+    photo: "images/harbor.webp",
+    link: "https://visitnewportbeach.com/beaches-and-parks/newport-harbor/",
+  },
+  {
+    name: "Fashion Island",
+    address: "401 Newport Center Dr",
+    description: "A destination within a destination—complete with ocean views—Fashion Island is home to a diverse collection of cafes, restaurants, boutiques, and department stores where you’ll find everything from casual comfort to contemporary fashion.",
+    photo: "images/fashion_island.webp",
+    link: "https://www.fashionisland.com/",
+  },
+  {
+    name: "Crystal Cove Beach",
+    address: "8471 N Coast Hwy",
+    description: "Crystal Cove State Park / Crystal Cove State Beach, located in Newport Beach, California, is best known for its rustic setting that stands in contrast to pristine, new homes and immaculate greens of the Pelican Hills Golf Club overlooking this wilderness oasis.",
+    photo: "images/crystal_cove.webp",
+    link: "https://visitnewportbeach.com/beaches-and-parks/crystal-cove-state-park/",
+  },
+  {
+    name: "University of California: Irvine",
+    address: "University of California, Irvine",
+    description: "Founded in 1965, the University of California, Irvine (UCI) is a top-ranked public research university dedicated to rigorous academics, cutting-edge research, and community service.",
+    photo: "images/uci.webp",
+    link: "https://uci.edu/",
+  },
+  {
+    name: "The Wedge",
+    address: "End of Balboa Boulevard",
+    description: "The Wedge Newport Beach is a hot spot for powerful and awesome surf. An area known for its shore-breaking waves, the Wedge is more a place to admire than to engage. Either way, this spot with the biggest swell in Southern California is a must see when you visit Newport Beach.",
+    photo: "images/wedge.webp",
+    link: "https://visitnewportbeach.com/beaches-and-parks/the-wedge/",
+  },
+];
+
+
---
+++ chamber/scripts/discover.js
@@ -0,0 +1,76 @@
+import { places } from "../data/places.mjs";
+
+//#region Navigation
+const menuBtn = document.querySelector("#ham-btn");
+const navMenu = document.querySelector("#nav-bar");
+
+menuBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+
+  if (navMenu.classList.contains("show")) {
+    menuBtn.textContent = "✕";
+  } else {
+    menuBtn.textContent = "☰";
+  }
+});
+//#endregion
+
+
+
+//#region Visit Message
+const message = document.getElementById("visit-message");
+const lastVisit = localStorage.getItem("lastVisit");
+const now = Date.now();
+
+if (!lastVisit) {
+  message.textContent = "Welcome! Let us know if you have any questions.";
+} else {
+  const days = Math.floor((now - lastVisit) / (1000 * 60 * 60 * 24));
+
+  if (days < 1) {
+    message.textContent = "Back so soon! Awesome!";
+  } else if (days === 1) {
+    message.textContent = "You last visited 1 day ago.";
+  } else {
+    message.textContent = `You last visited ${days} days ago.`;
+  }
+}
+
+localStorage.setItem("lastVisit", now);
+//#endregion
+
+
+
+//#region Discover Places
+const container = document.getElementById("discover-places");
+
+places.forEach(place => {
+  const card = document.createElement("div");
+  card.classList.add("place");
+
+  card.innerHTML = `
+    <h2>${place.name}</h2>
+    <div class="place-content">
+      <figure>
+        <img src="${place.photo}" alt="${place.name}">
+      </figure>
+      <div class="place-text">
+        <address>${place.address}</address>
+        <p>
+          ${place.description}
+        </p>
+      </div>
+    </div>
+    <a href="${place.link}" target="_blank" rel="noopener noreferrer">Learn more</a>
+  `;
+
+  container.appendChild(card);
+});
+//#endregion
+
+
+
+//#region Footer
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+//#endregion
